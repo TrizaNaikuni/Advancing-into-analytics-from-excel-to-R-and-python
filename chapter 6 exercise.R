fab900190c7fd3b2e6441efcd084140b25d8aed7
TrizaNaikuni <144409707+TrizaNaikuni@users.noreply.github.com>
2024-02-15 01:58:10 -0800

    Add files via upload

---
+++ chapter 6 exercise.R
@@ -0,0 +1,25 @@
+#1. I prefer chrome editor theme for clearer vision.
+
+#2a. Assign the sum of 1 and 4 as a.
+a<-sum(1,4)
+
+#2b.Assign the square root of a as b.
+b<-sqrt(a)
+
+#2c.Assign b minus 1 as d
+d<-b-1
+
+
+#2d.type of data is stored in d
+str(d)
+
+#2e.Is d greater than 2?
+d>2
+
+
+#3.Install the psych package
+install.packages('psych')
+
+# install.packages('psych')
+
+library(psych)
